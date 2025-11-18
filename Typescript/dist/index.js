c129be29e57cd3df0dc9c36ea0e5cfab2e664e44
Shruti Ranjan <95423325+Shruti10122002@users.noreply.github.com>
2025-11-18 08:52:01 +0530

    Add files via upload

---
+++ Typescript/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mathHelper_1 = require("./utils/mathHelper");
+console.log("Welcome Shruti 👋, your TypeScript is running!");
+console.log("Sum:", (0, mathHelper_1.add)(10, 20));
---
+++ Typescript/src/index.ts
@@ -0,0 +1,4 @@
+import { add } from "./utils/mathHelper";
+
+console.log("Welcome Shruti 👋, your TypeScript is running!");
+console.log("Sum:", add(10, 20));
